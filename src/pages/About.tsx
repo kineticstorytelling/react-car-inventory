5074ef31d43f0224ab1cb532fdb83a7a2fd14348
kineticstorytelling <kineticstorytelling@gmail.com>
2023-04-20 22:58:43 -0400

    'changed img to div'

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,5 @@
+import Background from '../assets/images/cars2.jpg'
+
 function About() {
   return (
     <div 
@@ -6,7 +8,12 @@ function About() {
       <h2 className="text-white p-3 text-6xl font-semibold">About</h2>
       <p className="text-white w-3/5 p-3">We understand that buying a car is a big decision, which is why we strive to make the process as easy and stress-free as possible. Our website is designed to help you find the car you want quickly and easily, with detailed information on each vehicle, including features, specifications, and pricing. You can even compare different models side by side to help you make the best decision.</p>
       </div>
-      <img className="w-2/5 mx-15" src="/src/assets/images/cars1.jpg" alt="" />
+      <div
+          style={{backgroundImage: `url(${ Background })`}} 
+          className='bg-cover w-1/2 h-screen'
+      >
+      </div>
+      {/* <img className="w-2/5 mx-15" src="/src/assets/images/cars1.jpg" alt="" /> */}
     </div>
   )
 }
